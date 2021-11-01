f0111f06fcef8db3a95479cc1aec8bf4dbfaa842
souryuu1003 <souryuu1003@gmail.com>
2021-11-01 21:42:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-w1qokx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-w1qokx)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  set = 0;
+
+  click(x) {
+    this.set = x;
+  }
+}
