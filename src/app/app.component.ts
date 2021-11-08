1117ddd60e57cd116bbd692c8db6c749a0b68049
souryuu1003 <souryuu1003@gmail.com>
2021-11-08 23:54:14 +0900

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,33 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  value = 0;
+  value = '';
+  setValue = '0';
+  setCalc = '';
   counter = -1;
-  setValue = [];
 
   keyUp(n) {
-    if (n != '=') {
-      this.counter++;
-      this.setValue.push(this.counter, n);
-    } else {
-      for (let i = 0; i < this.setValue.length; i++) {
-        console.log(this.setValue.indexOf(i));
+    if (0 < n && n < 10) {
+      this.value = this.value + n;
+    } else if (n != '=') {
+      this.setValue = this.value;
+      this.setCalc = n;
+      this.value = '';
+      console.log('5: ' + this.value);
+    } else if (this.setValue != null && this.setCalc != null && n == '=') {
+      if (this.setCalc == '+') {
+        this.value = parseInt(this.setValue) + parseInt(this.value) + '';
+      }
+      if (this.setCalc == '-') {
+        this.value = parseInt(this.setValue) - parseInt(this.value) + '';
+      }
+      if (this.setCalc == '*') {
+        this.value = parseInt(this.setValue) * parseInt(this.value) + '';
+      }
+      if (this.setCalc == '/') {
+        this.value = parseInt(this.setValue) / parseInt(this.value) + '';
       }
     }
+    return this.value;
   }
 }
